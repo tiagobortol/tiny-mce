d1c6cda9f3de7b53a7107cdfc23bfbfe5a91ae30
tiagobortol <tiagobortol@gmail.com>
2017-01-30 23:03:22 -0200

    alteração

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
                 editor.on('keypress', function (e) {
                     if (e.key === '$')
                     {
-                       
+                       alert('Letra d detectada');
                     }
                 });
             },
